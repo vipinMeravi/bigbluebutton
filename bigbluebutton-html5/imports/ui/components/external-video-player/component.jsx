57ef5ed33ee6a31f2d1c70144737653f532ca732
vipinMeravi <vipinmeravi4@gmail.com>
2020-06-04 14:14:50 +0100

    background changes

--- bigbluebutton-html5/imports/ui/components/external-video-player/component.jsx
+++ bigbluebutton-html5/imports/ui/components/external-video-player/component.jsx
@@ -427,12 +427,8 @@ class VideoPlayer extends Component {
       );
     } else {
       return (
-        <div
-          id="video-player"
-          data-test="videoPlayer"
-          ref={(ref) => { this.playerParent = ref; }}
-        >
-          <iframe class="col-lg-12 col-md-12 col-sm-12" src={videoUrl} />
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src={videoUrl} allowfullscreen  />
         </div>
       );    
     }
