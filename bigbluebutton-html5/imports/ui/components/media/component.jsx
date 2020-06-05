14f31f90bda08a768ecd9b657ac2b475d7956c97
vipinMeravi <vipinmeravi4@gmail.com>
2020-06-05 12:29:52 +0100

    split changes

--- bigbluebutton-html5/imports/ui/components/media/component.jsx
+++ bigbluebutton-html5/imports/ui/components/media/component.jsx
@@ -89,14 +89,23 @@ export default class Media extends Component {
       >
         <div
           className={!swapLayout ? contentClassName : overlayClassName}
-          // style={{
-          //   maxHeight: usersVideo.length < 1 || (webcamPlacement === 'floating') ? '100%' : '80%',
-          //   minHeight: '20%',
-          // }}
+          style={{
+            maxHeight: usersVideo.length < 1 || (webcamPlacement === 'floating') ? '100%' : '80%',
+            minHeight: '20%',
+          }}
         >
           {children}
-          {children_split ? {children_split} : null}
-        </div>        
+        </div>
+        {children_split ?
+        <div
+          className={!swapLayout ? contentClassName : overlayClassName}
+          style={{
+            maxHeight: usersVideo.length < 1 || (webcamPlacement === 'floating') ? '100%' : '80%',
+            minHeight: '20%',
+          }}
+        >
+          {children_split}
+        </div>  : null}      
         {usersVideo.length > 0 ? (
           <WebcamDraggable
             refMediaContainer={this.refContainer}
