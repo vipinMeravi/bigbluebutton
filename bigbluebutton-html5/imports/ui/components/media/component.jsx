54ef83d9800ba5ecf16acb4eefaf5135a71520bb
vipinMeravi <vipinmeravi4@gmail.com>
2020-06-08 09:41:30 +0100

    split changes 0806

--- bigbluebutton-html5/imports/ui/components/media/component.jsx
+++ bigbluebutton-html5/imports/ui/components/media/component.jsx
@@ -86,15 +86,12 @@ export default class Media extends Component {
         id="container"
         className={cx(styles.container)}
         ref={this.refContainer}
-        style={{
-          display: children_split ?contents:block,
-        }}
       >
         <div
           className={!swapLayout ? contentClassName : overlayClassName}
           style={{
             maxHeight: usersVideo.length < 1 || (webcamPlacement === 'floating') ? '100%' : '80%',
-            minHeight: '20%', width: children_split ?"50%":null,
+            minHeight: '20%', width: "50%",
           }}
         >
           {children}
