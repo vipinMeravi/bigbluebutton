896a35ff31f5f7ccdd763e6a952b2d0f8f12ba6a
fayyaz <fayyaz@codezilla.io>
2020-06-11 07:32:37 +0100

    split screen 0906

--- bigbluebutton-html5/imports/ui/components/actions-bar/container.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/container.jsx
@@ -40,29 +40,35 @@ class ActionsBarContainer extends PureComponent {
 
 const POLLING_ENABLED = Meteor.settings.public.poll.enabled;
 
-export default withTracker(() => ({
-  amIPresenter: Service.amIPresenter(),
-  amIModerator: Service.amIModerator(),
-  stopExternalVideoShare: ExternalVideoService.stopWatching,
-  handleExitVideo: () => VideoService.exitVideo(),
-  handleJoinVideo: () => VideoService.joinVideo(),
-  handleShareScreen: onFail => shareScreen(onFail),
-  handleUnshareScreen: () => unshareScreen(),
-  isVideoBroadcasting: isVideoBroadcasting(),
-  screenSharingCheck: getFromUserSettings('bbb_enable_screen_sharing', Meteor.settings.public.kurento.enableScreensharing),
-  enableVideo: getFromUserSettings('bbb_enable_video', Meteor.settings.public.kurento.enableVideo),
-  isLayoutSwapped: getSwapLayout() && shouldEnableSwapLayout(),
-  toggleSwapLayout: MediaService.toggleSwapLayout,
-  handleTakePresenter: Service.takePresenterRole,
-  currentSlidHasContent: PresentationService.currentSlidHasContent(),
-  parseCurrentSlideContent: PresentationService.parseCurrentSlideContent,
-  isSharingVideo: Service.isSharingVideo(),
-  screenShareEndAlert,
-  screenshareDataSavingSetting: dataSavingSetting(),
-  isCaptionsAvailable: CaptionsService.isCaptionsAvailable(),
-  isMeteorConnected: Meteor.status().connected,
-  isPollingEnabled: POLLING_ENABLED,
-  isThereCurrentPresentation: Presentations.findOne({ meetingId: Auth.meetingID, current: true },
-    { fields: {} }),
-  allowExternalVideo: Meteor.settings.public.externalVideoPlayer.enabled,
-}))(injectIntl(ActionsBarContainer));
+export default withTracker((props) => {
+  console.log('props inside the with tracker ================>')
+  console.log(props)
+  console.log('props inside the with tracker ================>')
+  return {
+
+    amIPresenter: Service.amIPresenter(),
+    amIModerator: Service.amIModerator(),
+    stopExternalVideoShare: ExternalVideoService.stopWatching,
+    handleExitVideo: () => VideoService.exitVideo(),
+    handleJoinVideo: () => VideoService.joinVideo(),
+    handleShareScreen: onFail => shareScreen(onFail),
+    handleUnshareScreen: () => unshareScreen(),
+    isVideoBroadcasting: isVideoBroadcasting(),
+    screenSharingCheck: getFromUserSettings('bbb_enable_screen_sharing', Meteor.settings.public.kurento.enableScreensharing),
+    enableVideo: getFromUserSettings('bbb_enable_video', Meteor.settings.public.kurento.enableVideo),
+    isLayoutSwapped: getSwapLayout() && shouldEnableSwapLayout(),
+    toggleSwapLayout: MediaService.toggleSwapLayout,
+    handleTakePresenter: Service.takePresenterRole,
+    currentSlidHasContent: PresentationService.currentSlidHasContent(),
+    parseCurrentSlideContent: PresentationService.parseCurrentSlideContent,
+    isSharingVideo: Service.isSharingVideo(),
+    screenShareEndAlert,
+    screenshareDataSavingSetting: dataSavingSetting(),
+    isCaptionsAvailable: CaptionsService.isCaptionsAvailable(),
+    isMeteorConnected: Meteor.status().connected,
+    isPollingEnabled: POLLING_ENABLED,
+    isThereCurrentPresentation: Presentations.findOne({ meetingId: Auth.meetingID, current: true },
+      { fields: {} }),
+    allowExternalVideo: Meteor.settings.public.externalVideoPlayer.enabled,
+  }
+})(injectIntl(ActionsBarContainer));
