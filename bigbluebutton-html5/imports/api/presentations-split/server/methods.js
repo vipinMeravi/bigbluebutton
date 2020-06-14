611041e9e6db13456eaedf442aca4a11440c08d6
vipinMeravi <vipinmeravi4@gmail.com>
2020-06-14 16:02:23 +0100

    split changes

--- bigbluebutton-html5/imports/api/presentations-split/server/methods.js
+++ bigbluebutton-html5/imports/api/presentations-split/server/methods.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import removePresentation from './methods/removePresentation';
+import removePresentation from './methods/removePresentations';
 import setPresentation from './methods/setPresentation';
 import setPresentationDownloadable from './methods/setPresentationDownloadable';
 
