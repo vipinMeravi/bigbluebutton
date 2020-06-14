7a6f81a0deb7831fef243a745539df2f5db6352b
vipinMeravi <vipinmeravi4@gmail.com>
2020-06-14 15:44:11 +0100

    split changes

--- bigbluebutton-html5/imports/api/presentations-split/server/methods/removePresentation.js
+++ bigbluebutton-html5/imports/api/presentations-split/server/methods/removePresentation.js
@@ -2,7 +2,7 @@ import RedisPubSub from '/imports/startup/server/redis';
 import { check } from 'meteor/check';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 
-export default function removePresentation(presentationId, podId) {
+export default function removePresentations(presentationId, podId) {
   const REDIS_CONFIG = Meteor.settings.private.redis;
   const CHANNEL = REDIS_CONFIG.channels.toAkkaApps;
   const EVENT_NAME = 'RemovePresentationPubMsg';
