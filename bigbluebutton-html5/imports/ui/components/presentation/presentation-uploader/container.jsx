eac47e2fc7214f62d6b903827e15af1c09b519ff
fayyaz <fayyaz@codezilla.io>
2020-06-19 10:48:38 +0100

    multidocuments

--- bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/container.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/container.jsx
@@ -27,7 +27,7 @@ export default withTracker(() => {
       currentPresentations,
       presentations,
       PRESENTATION_CONFIG.uploadEndpoint,
-      'DEFAULT_PRESENTATION_POD_SPLIT',
+      'DEFAULT_PRESENTATION_POD',
     ),
     dispatchDisableDownloadable,
     dispatchEnableDownloadable,
