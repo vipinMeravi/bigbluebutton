5c7983ccaeb1bd10ea10259851bef45882ed3862
fayyaz <fayyaz@codezilla.io>
2020-06-19 10:21:41 +0100

    multidocuments

--- bigbluebutton-html5/imports/api/presentation-pods-split/index.js
+++ bigbluebutton-html5/imports/api/presentation-pods-split/index.js
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import PresentationsPodSplit from '/imports/api/presentations-pods';
 
-const PresentationPods = new Mongo.Collection('presentation-pods');
+const PresentationPods = PresentationsPodSplit
+// if (Meteor.isServer) {
+//   // types of queries for the presentation pods:
+//   // 1. meetingId, podId  ( 4 )
 
-if (Meteor.isServer) {
-  // types of queries for the presentation pods:
-  // 1. meetingId, podId  ( 4 )
-
-  PresentationPods._ensureIndex({ meetingId: 1, podId: 1 });
-}
+//   PresentationPods._ensureIndex({ meetingId: 1, podId: 1 });
+// }
 
 export default PresentationPods;
