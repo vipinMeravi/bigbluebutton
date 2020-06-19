5f1fbd76f2a6c9e8862c9315d45a13a7943ef693
fayyaz <fayyaz@codezilla.io>
2020-06-19 10:26:20 +0100

    multidocuments

--- bigbluebutton-html5/imports/api/presentation-pods/index.js
+++ bigbluebutton-html5/imports/api/presentation-pods/index.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
-const PresentationPods = new Mongo.Collection('presentation-pods-split');
+const PresentationPods = new Mongo.Collection('presentation-pods');
 
 if (Meteor.isServer) {
   // types of queries for the presentation pods:
