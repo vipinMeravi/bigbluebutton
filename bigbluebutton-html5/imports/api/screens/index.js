04b799f1e5ae8eab32c30b5f7024d2544212b791
fayyaz <fayyaz@codezilla.io>
2020-06-23 07:33:45 +0100

    Screen logic

--- bigbluebutton-html5/imports/api/screens/index.js
+++ bigbluebutton-html5/imports/api/screens/index.js
@@ -1,11 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-
+import { extractCredentials } from '/imports/api/common/server/helpers';
 const Screens = new Mongo.Collection('screens')
 
 if(Meteor.isServer){
   Screens._ensureIndex({
     meetingId: 1
   });
+  const { meetingId } = extractCredentials(this.userId);
+  
   const fullscreenSelector = {
     meetingId,
     screen_value: 'fullscreen'
